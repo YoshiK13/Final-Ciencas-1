f7a0dc00dd12297dbe46dc45775f6189cb512b16
Josh <128151012+YoshiK13@users.noreply.github.com>
2025-12-01 22:28:24 +0000

    Añadir base de app y añadir init pacquete de logica

---
+++ Logica/__init__.py
@@ -0,0 +1,1 @@
+# Init para el paquete Logica
---
+++ Visual/App.py
@@ -0,0 +1,14 @@
+# Implementacion del getsor de estudiantes con arboles AVL
+# de manera visual por consola
+
+# Integrantes:
+# Davidson Esfleider Sanchez Gordillo - 20231020183
+# Diego Felipe Diaz Roa - 20201020147
+# Dania Lizeth Guzmán Triviño - 20221020061
+# Joshoa Alarcon Sanchez - 20221020013
+
+from Logica.Estudiante import Estudiante
+from Logica.Arboles import NodoAVL
+from Logica.Gestor import GestorEstudiantes
+
+# TO-DO: hacer el codigo visual por consola
